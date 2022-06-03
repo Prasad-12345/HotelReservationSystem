e7013e6ce848650f51e3a80f6fdb70a110c150a2
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-03 17:18:43 +0530

    Added: Test method for cheapest hotel

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -20,15 +20,18 @@ public class HotelReservationTest {
         Assert.assertEquals(hotelReservation.hotelList.size(), 3);
     }
 
-//    @Test
-/*    public void givenHotelDetails_shouldReturnCheapestHotel(){
+    /*
+    * Test case to check cheapest hotel
+     */
+    @Test
+    public void givenHotelDetails_shouldReturnCheapestHotel(){
         HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("lakewood", 3, 110);
-        hotelReservation.addHotel("bridgewood", 4, 160);
-        hotelReservation.addHotel("ridgewood", 5, 220);
+        hotelReservation.addHotel("Lakewood", 3, 110, 90);
+        hotelReservation.addHotel("Bridgewood", 4, 160, 50);
+        hotelReservation.addHotel("ridgewood", 5, 220, 150);
         LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
-        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
-        hotelReservation.getCheapestHotel(startDate, endDate);
-        Assert.assertEquals(hotelReservation.hotelList.get(0).getHotelName(), "lakewood");
-    }*/
+        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);
+        String hotelName = hotelReservation.getCheapestHotel(startDate, endDate);
+        Assert.assertEquals("Lakewood", hotelName);
+    }
 }
