5bf3596277d78adfe39b79207c6e202317d90da9
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-03 14:35:24 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservationsystem;
+/*
+ *Author: Prasad
+ *Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer
+ */
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Hotel Reservation System");
+    }
+}
